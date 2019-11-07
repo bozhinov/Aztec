3d6f77f4ccfa494e7f8086841d789a744628b97d
Momchil Bozhinov <momchil@bojinov.info>
2019-11-07 11:54:53 +0200

    Update BitMatrix.php
    That notepad++ is starting to get on my nerves

--- Aztec/Utils/BitMatrix.php
+++ Aztec/Utils/BitMatrix.php
@@ -4,31 +4,31 @@ namespace Aztec\Utils;
 
 class BitMatrix
 {
-    private $matrix;
+	private $matrix;
 	private $width;
 
-    public function __construct($width)
-    {
-        $this->matrix = [new \SplFixedArray($width), new \SplFixedArray($width)];
+	public function __construct($width)
+	{
+		$this->matrix = [new \SplFixedArray($width), new \SplFixedArray($width)];
 		$this->width = $width;
-    }
+	}
 
-    public function set($x, $y, $bit = 1)
-    {
-        $this->matrix[$x][$y] = $bit;
-    }
+	public function set($x, $y, $bit = 1)
+	{
+		$this->matrix[$x][$y] = $bit;
+	}
 
-    public function get($x, $y)
-    {
+	public function get($x, $y)
+	{
 		if (!isset($this->matrix[$x][$y])){
 			return FALSE;
 		} else {
 			return $this->matrix[$x][$y];
 		}
-    }
+	}
 
-    public function getWidth()
-    {
-        return $this->width;
-    }
+	public function getWidth()
+	{
+		return $this->width;
+	}
 }
