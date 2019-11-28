85ef1997197eaf06781bedc39471d89151c25aa2
Momchil Bozhinov <momchil@bojinov.info>
2019-11-28 10:57:41 +0200

    * Fix #1

--- Aztec/Encoder/BinaryDataEncoder.php
+++ Aztec/Encoder/BinaryDataEncoder.php
@@ -1,21 +1,5 @@
 <?php
 
-/*
- * Copyright 2013 Metzli authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 namespace Aztec\Encoder;
 
 use Aztec\BitArray;
@@ -28,26 +12,25 @@ class BinaryDataEncoder
     {
         $result = new BitArray();
 
-        while (strlen($data) >= 32) {
-            $chunkLength = min(strlen($data), (2048 + 32 - 1));
+		$data = array_values(unpack('C*', $data));
+		$len = count($data);
+
+		# Used to split the string in (2048 + 32 - 1) long pieces
+		# Barcode can't store that much anyway
+		if ($len >= 32) {
             $result->append(31, 5);
             $result->append(0, 5);
-            $result->append(($chunkLength - 32), 11);
-			$bytes = substr($data, 0, $chunkLength);
-			for ($i = 0; $i < $chunkLength; $i++) {
-				$result->append(ord($bytes[$i]), 8);
-			}
-            $data = substr($data, $chunkLength);
-        }
-		
-		$len = strlen($data);
-        if ($len > 0) {
+			# Used to be $len - 32 but that resulted 
+			# in AK at the end of the decoded string
+            $result->append(($len - 31), 11);
+        } else {
             $result->append(31, 5);
             $result->append($len, 5);
-			for ($i = 0; $i < $len; $i++) {
-				$result->append(ord($data[$i]), 8);
-			}
         }
+
+		foreach($data as $ord){
+			$result->append($ord, 8);
+		}
 
         return $result;
     }
