1042ea47743f3582958d6fab9ac34d1fdd02ffc5
Momchil Bozhinov <momchil@bojinov.info>
2019-11-29 17:31:09 +0200

    Update Token.php

--- Aztec/Encoder/Token.php
+++ Aztec/Encoder/Token.php
@@ -1,21 +1,5 @@
 <?php
 
-/*
- * Copyright 2013 Metzli and ZXing authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 namespace Aztec\Encoder;
 
 class Token
@@ -51,34 +35,36 @@ class Token
 	{
 		return $this->previous;
 	}
-	
+
 	public function setHistory(array $history)
 	{
 		$this->previous = $history;
 	}
-	
+
 	public function addtoHistory(array $previous)
 	{
 		$this->previous[] = $previous;
 	}
 
-	public function add($value, $bitCount)
+	private function instantiate($value, $bitCount, $type)
 	{
-		$token = new self([$value, $bitCount, 0]);
-		$token->setState($this->mode, $this->shiftByteCount, $this->bitCount);
+		$bc = ($type == 1) ? 0 : $this->shiftByteCount;
+
+		$token = new self();
+		$token->setState($this->mode, $bc, $this->bitCount);
 		$token->setHistory($this->previous);
-		$token->addtoHistory([$value, $bitCount, 0]);
+		$token->addtoHistory([$value, $bitCount, $type]);
 
 		return $token;
 	}
 
+	public function add($value, $bitCount)
+	{
+		return $this->instantiate($value, $bitCount, 0);
+	}
+
 	public function addBinaryShift($value, $bitCount)
 	{
-		$token = new self();
-		$token->setState($this->mode, 0, $this->bitCount);
-		$token->setHistory($this->previous);
-		$token->addtoHistory([$value, $bitCount, 1]);
-
-		return $token;
+		return $this->instantiate($value, $bitCount, 1);
 	}
 }
