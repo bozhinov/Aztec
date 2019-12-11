fc4b19bc4ef98237894935b74748996fb5b60210
Momchil Bozhinov <momchil@bojinov.info>
2019-12-11 12:49:21 +0200

    Update ReedSolomon.php
    Used to be 13kb it is 4kb now
    It's time we remove the copyright

--- Aztec/Encoder/ReedSolomon.php
+++ Aztec/Encoder/ReedSolomon.php
@@ -1,21 +1,5 @@
 <?php
 
-/*
-* Copyright 2013 Metzli and ZXing authors
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
 namespace Aztec\Encoder;
 
 use \Aztec\azException;
@@ -36,10 +20,6 @@ class ReedSolomon
 
 	public function __construct($wordSize)
 	{
-		if (!isset($this->gfTable[$wordSize])){
-			throw azException::InvalidInput("Word size of $wordSize was unexpected");
-		}
-
 		$this->size = pow(2, $wordSize);
 		$this->initialize($this->gfTable[$wordSize]);
 	}
@@ -109,7 +89,7 @@ class ReedSolomon
 		}
 
 		if ($this->isZero($product)) {
-			throw azException::InvalidInput('Divide by 0');
+			throw azException::EncoderError('Divide by 0');
 		}
 
 		return $this->getPoly($product);
